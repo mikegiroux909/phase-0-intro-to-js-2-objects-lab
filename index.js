480ebf017aa0a3380824e6042bafd88925da4dde
mike giroux <michaelgiroux909@gmail.com>
2023-01-10 14:11:07 -0800

    commit tree 849ca1902a5e7cd0785de542b32359dc5256159a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {name : "mike", streetAddress : "atwood"};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+      };
+} 
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] =value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+} 
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
